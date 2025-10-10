dd94ecdb44f0318d82ae95e3afbf14c717c80833
shai-yona <shaiyonabusiness@example.com>
2025-10-10 13:52:16 +0300

    initial tomcat server

---
+++ s-emulator-server/src/HelloWorldServlet.java
@@ -0,0 +1,13 @@
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "HelloServlet", urlPatterns = "/hello")
+public class HelloWorldServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.getWriter().println("Hello World !");
+    }
+}
